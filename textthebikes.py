a8c3287f5ae891b594381bf58f3ae0363aa440b2
Maia <maia.mcc@gmail.com>
2015-03-09 12:19:39 -0400

    added gitignore, fixed spacing

--- textthebikes.py
+++ textthebikes.py
@@ -7,10 +7,10 @@ LNG_DEGREES_DIFF = .004
 NUM_STATIONS = 5
 
 def get_all_docks():
-	station_json=requests.get("http://www.citibikenyc.com/stations/json").content
+	station_json = requests.get("http://www.citibikenyc.com/stations/json").content
 	#this gives us a list of dictionaries. each dictionary is a station+ info
-	station_list=json.loads(station_json)["stationBeanList"]
-	station_dict={}
+	station_list = json.loads(station_json)["stationBeanList"]
+	station_dict = {}
 	for station in station_list:
 		station_id = station["id"]
 		del station["id"]
@@ -24,26 +24,26 @@ def find_near_stations(lat, lng, station_dict, iterations=1):
 
 	if iterations > 100:
 		raise NoStationsError("I guess there are no stations near you. Womp.")
-	near_stations={}
+	near_stations = {}
 	for station_id, station_info in station_dict.iteritems():
-		if (lat-iterations*LAT_DEGREES_DIFF)<=station_info["latitude"]<=(lat+iterations*LAT_DEGREES_DIFF) and \
-			(lng-iterations*LNG_DEGREES_DIFF)<=station_info["longitude"]<=(lng+iterations*LNG_DEGREES_DIFF):
+		if (lat - iterations * LAT_DEGREES_DIFF) <= station_info["latitude"] <= (lat + iterations * LAT_DEGREES_DIFF) and \
+			(lng - iterations * LNG_DEGREES_DIFF) <= station_info["longitude"] <= (lng + iterations * LNG_DEGREES_DIFF):
 			near_stations[station_id] = station_info
 
 	clean_stations(near_stations)
 	if len(near_stations) < NUM_STATIONS:
-		near_stations=find_near_stations(lat, lng, station_dict, iterations=iterations+1)
+		near_stations = find_near_stations(lat, lng, station_dict, iterations=iterations+1)
 	print iterations
 	return near_stations
 
 def clean_stations(near_stations,bikes_only=False, docks_only=False):
 	for station_id, station_info in near_stations.iteritems():
-		if station_info["statusValue"] !="In Service":
+		if station_info["statusValue"] != "In Service":
 			del near_stations[station_id]
 
 	# if status not 'good', remove
 	# IF LOOKING FOR JUST BIKES/DOCKS: remove if no bikes/docks
-	# if not enough stations, 
+	# if not enough stations,
 
 
 
